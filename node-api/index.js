d278db25a1d0adc47297b1c185411fab1dc05dc7
plitche <kysu728@naver.com>
2022-07-02 21:27:27 +0900

    7. 노드로 만나는 Hello World - Hello World 노드버전

--- node-api/index.js
+++ node-api/index.js
@@ -1,6 +1,14 @@
-const fs = require('fs');
+const http = require('http');
 
-// const data = fs.readFileSync('./data.txt', 'utf-8');
-const data = fs.readFile('./data.txt', 'utf-8', function(err, data) {
-    console.log(data);
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World!\n');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
