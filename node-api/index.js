99e6c123946906ed7334d7d9e8c80d4a7dad8cbf
plitche <kysu728@naver.com>
2022-07-02 21:35:34 +0900

    노드로 만나는 Hello World - 헬로월드 코드읽기

--- node-api/index.js
+++ node-api/index.js
@@ -1,14 +1,16 @@
-const http = require('http');
+const http = require('http'); // 모듈 가져오기
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
-const server = http.createServer((req, res) => {
+const server = http.createServer((req, res) => { // 화살표 함수 es6
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World!\n');
+  res.end('Hello World!\n'); // 클라이언트에게 보내줌
 });
 
-server.listen(port, hostname, () => {
+server.listen(port, hostname, () => { // 서버를 요청 대기상태로 만들어주는 함수
   console.log(`Server running at http://${hostname}:${port}/`);
 });
+
+// 요청 대기상태 : 서버가 클라이언트의 요청을 받기 위해서 대기하는 상태
