eb9a090aa2b0c040b90f02f2a1b0eeb74fcc59b0
plitche <kysu728@naver.com>
2022-07-03 15:44:54 +0900

    13. 익스프레스(ExpressJS) 기초 - 미들웨어 실행 순서

--- node-api/index.js
+++ node-api/index.js
@@ -6,7 +6,13 @@ function logger(req, res, next) {
   next(); // next함수를 호출해야만이 다음 로직을 실행한다.
 }
 
+function logger2(req, res, next) {
+  console.log('i am logger2');
+  next();
+}
+
 app.use(logger);
+app.use(logger2);
 
 app.listen(3000, function() {
   console.log('Server is running');
