51b4da8952ac50ef07ff79b2352a0b71dbd8ee19
plitche <kysu728@naver.com>
2022-07-03 17:45:06 +0900

    22. REST API란 - 첫번째 API 만들기: 사용자 목록 조회 API

--- node-api/index.js
+++ node-api/index.js
@@ -1,15 +1,18 @@
-const express = require('express')
-const app = express()
-const port = 3000
+var express = require('express')
+var app = express()
+var morgan = require('morgan')
+var port = 3000
+var users = [
+  {id: 1, name: 'aplice'},
+  {id: 2, name: 'bek'},
+  {id: 3, name: 'chris'}
+];
+
+app.use(morgan('dev'));
 
 // 라우팅 설정
-app.get('/', (req, res) => { // 익스프레스 어플리케이션의 메소드(HTTP 메서드)
-  res.send('Hello World!')
-})
-
-app.post('/users', function(req, res) {
-  // create user...
-  res.send('user');
+app.get('/users', (req, res) => { // 익스프레스 어플리케이션의 메소드(HTTP 메서드)
+  res.send(users);
 })
 
 app.listen(port, () => {
