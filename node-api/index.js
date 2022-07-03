a80b11abcdc2536cc500409a30d3d7dd1c908e45
plitche <kysu728@naver.com>
2022-07-03 15:36:56 +0900

    12. 익스프레스(ExpressJS) 기초 - 미들웨어 만들기

--- node-api/index.js
+++ node-api/index.js
@@ -1,5 +1,13 @@
 const express = require('express');
 const app = express();
+
+function logger(req, res, next) {
+  console.log('i am logger');
+  next(); // next함수를 호출해야만이 다음 로직을 실행한다.
+}
+
+app.use(logger);
+
 app.listen(3000, function() {
   console.log('Server is running');
 })
