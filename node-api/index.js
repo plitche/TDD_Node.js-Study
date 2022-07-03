f3aa4bfdf73d7b870db5b7a6a636480aa670e307
plitche <kysu728@naver.com>
2022-07-03 17:18:37 +0900

    20. REST API란 - 요청형식

--- node-api/index.js
+++ node-api/index.js
@@ -3,8 +3,13 @@ const app = express()
 const port = 3000
 
 // 라우팅 설정
-app.get('/', (req, res) => {
+app.get('/', (req, res) => { // 익스프레스 어플리케이션의 메소드(HTTP 메서드)
   res.send('Hello World!')
+})
+
+app.post('/users', function(req, res) {
+  // create user...
+  res.send('user');
 })
 
 app.listen(port, () => {
