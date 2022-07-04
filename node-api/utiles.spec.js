1a68002ac1eec2b2e27a94b51013da0b7123d407
plitche <kysu728@naver.com>
2022-07-04 22:36:55 +0900

    26. 테스트 주도 개발(TDD) - 모카(mocha) 2

---
+++ node-api/utiles.spec.js
@@ -0,0 +1,7 @@
+const utils = require('./utiles.js');
+
+describe('utils.js모듈의 capitalize() 함수는 ' , () => { // test suit
+    it('문자열의 첫번째 문자를 대문자로 변환한다.', () => { // test case
+        // test code
+    })
+})
