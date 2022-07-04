31dd4fb10d3a990d99f22051005c85f115d65586
plitche <kysu728@naver.com>
2022-07-04 22:50:46 +0900

    28. 테스트 주도 개발(TDD) - 슈드(should)

--- node-api/utiles.spec.js
+++ node-api/utiles.spec.js
@@ -1,10 +1,12 @@
 const utils = require('./utiles.js');
-const assert = require('assert');
+// const assert = require('assert');
+const should = require('should');
 
 describe('utils.js모듈의 capitalize() 함수는 ' , () => { // test suit
     it('문자열의 첫번째 문자를 대문자로 변환한다.', () => { // test case
         // test code
         const result = utils.capitialize('hello');
-        assert.equal(result, 'Hello');
+        //assert.equal(result, 'Hello');
+        result.should.be.equal('Hello');
     })
 })
