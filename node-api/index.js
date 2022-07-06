3551cd220579215ad9b1af62bcc589fd1df16b93
plitche <kysu728@naver.com>
2022-07-06 23:03:27 +0900

    39. TDD로 하는 API 서버 개발 - bodyParser 모듈

--- node-api/index.js
+++ node-api/index.js
@@ -1,6 +1,7 @@
 var express = require('express')
 var app = express()
 var morgan = require('morgan')
+var bodyParser = require('body-parser')
 var port = 3000
 var users = [
   {id: 1, name: 'aplice'},
@@ -9,6 +10,8 @@ var users = [
 ];
 
 app.use(morgan('dev'));
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({extended: true})); // for parsing application/x-www-form-urlencoded
 
 // 라우팅 설정
 app.get('/users', (req, res) => { // 익스프레스 어플리케이션의 메소드(HTTP 메서드)
@@ -36,6 +39,14 @@ app.delete('/usres/:id', (req, res) => {
   users = users.filter(user => user.id !== id);
   // id가 삭제할 id와 같이 않은 아이들만 filter해서 새로운 배열을 기존 배열과 바꾼다.
   res.status(204).end();
+})
+
+app.post('/users', (req, res) => {
+  const name = req.body.name;
+  const id = Date.now();
+  const user = {id, name};
+  users.push(user);
+  res.status(201).json(user);
 })
 
 app.listen(port, () => {
