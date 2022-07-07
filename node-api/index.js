039702b416e4a4982406b20aec9df426d80d13c4
plitche <kysu728@naver.com>
2022-07-08 00:23:46 +0900

    46. 코드 리펙토링 - 테스트 환경 개선

--- node-api/index.js
+++ node-api/index.js
@@ -5,15 +5,13 @@ var bodyParser = require('body-parser')
 var port = 3000
 var user = require('./api/user/index.js'); // index.js는 생략 가능
 
-app.use(morgan('dev'));
+if (process.env.NODE_ENV !== 'test') {
+  app.use(morgan('dev'));
+}
+
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({extended: true})); // for parsing application/x-www-form-urlencoded
 
 app.use('/users', user); // /users로 들어오는 모든 요청은 user가 담당한다.
-
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
 
 module.exports = app;
