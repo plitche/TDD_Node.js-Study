67a7e268056a8f50c646af2df2a65d1dc85d42fa
plitche <kysu728@naver.com>
2022-07-08 00:11:05 +0900

    44. 코드 리펙토링 - 컨트롤러 함수로 분리

--- node-api/api/user/index.js
+++ node-api/api/user/index.js
@@ -1,71 +1,14 @@
 // 라우팅 설정 로직
 const express = require('express');
 const router = express.Router();
+const ctrl = require('./user.ctrl');
 
-var users = [
-    {id: 1, name: 'aplice'},
-    {id: 2, name: 'bek'},
-    {id: 3, name: 'chris'}
-    ];
-
-    // /users라는 경로를 루트 폴더의 index.js에 설정해놓았기 때문에 알아서 들어온다.
- router.get('/', (req, res) => { // 익스프레스 어플리케이션의 메소드(HTTP 메서드)
-    req.query.limit = req.query.limit || 10;
-    const limit = parseInt(req.query.limit, 10); // "2" (정수, 진수)
-    if (Number.isNaN(limit)) {
-      return res.status(400)
-    }
-    res.send(users.slice(0, limit));
- });
-  
- router.get('/:id', function(req, res) {
-    const id = parseInt(req.params.id, 10);
-    if(id.isNaN) return res.status(400).end();
-  
-    const user = users.filter((user) => user.id === id)[0];
-    if (!user) return res.status(400).end();
-    res.json(user);
-  });
-  
-  router.delete('/:id', (req, res) => {
-    const id = parseInt(req.params.id, 10);
-    if (Number.isNaN(id)) return res.status(400).end();
-  
-    users = users.filter(user => user.id !== id);
-    // id가 삭제할 id와 같이 않은 아이들만 filter해서 새로운 배열을 기존 배열과 바꾼다.
-    res.status(204).end();
-  });
-  
-  router.post('/', (req, res) => {
-    const name = req.body.name;
-    if (!name) return res.status(400),end();
-  
-    const isConflic = users.filter(user => user.name === name).length
-    if (isConflic) return res.status(409).end();
-  
-    const id = Date.now();
-    const user = {id, name};
-    users.push(user);
-    res.status(201).json(user);
-  });
-  
-  router.put('/:id', (req, res) => {
-    const id = parseInt(req.params.id);
-    if (Number.isNaN(id)) return res.status(400).end();
-  
-    const name = req.body.name;
-    if (!name) return res.status(400).end();
-  
-    const isConflict = users.filter(user => user.name === name).length;
-    if (isConflict) return res.status(409).end();
-  
-    const user = users.filter(user => user.id === id)[0];
-    if (!user) return res.status(404).end();
-  
-    user.name = name;
-  
-    res.json(user);
-  });
+// /users라는 경로를 루트 폴더의 index.js에 설정해놓았기 때문에 알아서 들어온다.
+ router.get('/', ctrl.index); // /users의 routing에 대한 컨트롤러 함수를 바인딩
+ router.get('/:id', ctrl.show);
+ router.delete('/:id', ctrl.destroy);
+ router.post('/', ctrl.create);
+ router.put('/:id', ctrl.update);
 
   // root에 있는 index.js에서 써야하기 때문에 module로 export한다.
   module.exports = router;
--- node-api/api/user/user.ctrl.js
+++ node-api/api/user/user.ctrl.js
@@ -1,1 +1,75 @@
 // api 로직
+var users = [
+    {id: 1, name: 'aplice'},
+    {id: 2, name: 'bek'},
+    {id: 3, name: 'chris'}
+    ];
+
+const index = function (req, res) { // 익스프레스 어플리케이션의 메소드(HTTP 메서드)
+    req.query.limit = req.query.limit || 10;
+    const limit = parseInt(req.query.limit, 10); // "2" (정수, 진수)
+    if (Number.isNaN(limit)) {
+      return res.status(400)
+    }
+    res.send(users.slice(0, limit));
+ };
+
+const show = function(req, res) {
+    const id = parseInt(req.params.id, 10);
+    if(id.isNaN) return res.status(400).end();
+  
+    const user = users.filter((user) => user.id === id)[0];
+    if (!user) return res.status(400).end();
+    res.json(user);
+  };
+
+const destroy = function(req, res) {
+    const id = parseInt(req.params.id, 10);
+    if (Number.isNaN(id)) return res.status(400).end();
+  
+    users = users.filter(user => user.id !== id);
+    // id가 삭제할 id와 같이 않은 아이들만 filter해서 새로운 배열을 기존 배열과 바꾼다.
+    res.status(204).end();
+  }
+
+const create = function (req, res) {
+    const name = req.body.name;
+    if (!name) return res.status(400),end();
+  
+    const isConflic = users.filter(user => user.name === name).length
+    if (isConflic) return res.status(409).end();
+  
+    const id = Date.now();
+    const user = {id, name};
+    users.push(user);
+    res.status(201).json(user);
+  }
+
+  const update = function (req, res) {
+    const id = parseInt(req.params.id);
+    if (Number.isNaN(id)) return res.status(400).end();
+  
+    const name = req.body.name;
+    if (!name) return res.status(400).end();
+  
+    const isConflict = users.filter(user => user.name === name).length;
+    if (isConflict) return res.status(409).end();
+  
+    const user = users.filter(user => user.id === id)[0];
+    if (!user) return res.status(404).end();
+  
+    user.name = name;
+  
+    res.json(user);
+  }
+
+//  module.exports = {
+//     index: index,
+//     show: show,
+//     destroy: destroy,
+//     create: create,
+//     update: update
+//  }
+
+ module.exports = {index, show, destroy, create, update};
+
